e2e2a44a430ef63fe7503523b5b270db0f7a16bf
RNDaniel <rabbyn@gmail.com>
2022-03-10 15:37:24 +0100

    ciklusok

---
+++ input_loop.py
@@ -0,0 +1,18 @@
+# name = input("mi a neved?")
+# f = int(input("hányszor írja ki?"))
+#
+# for i in range(f):
+#     print(str(i+1)+" " + name)
+
+#for i in set(["Január","február",2,2,"március"]):
+#    print(i)
+
+
+numbers=[1,2,3,4,5,6,7]
+sum = 0
+for i in numbers:
+    sum += i
+print(sum)
+
+
+
---
+++ loops.py
@@ -0,0 +1,13 @@
+for i in range(5):
+    print("hello python")
+    print(i)
+
+for i in range(6,10):
+    print(i)
+    print("----------")
+
+for i in [2,7,11,22]:
+    print(i)
+
+for name in ["john","jack","jane"]:
+    print(name)
