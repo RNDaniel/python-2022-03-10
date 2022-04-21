8a7634e1326c70724701eee1630ca8e332162b9e
RNDaniel <rabbyn@gmail.com>
2022-04-21 21:57:38 +0200

    teszt

--- locations_test.py
+++ locations_test.py
@@ -10,63 +10,68 @@ from locations_po import LocationsPage
 from locations_db import create_location, delete_all_locations
 from read_locations import read_locations
 
+
 @pytest.fixture
 def driver():
-    #feldobja a böngészőt
+    # feldobja a böngészőt
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    
+
     delete_all_locations()
     return driver
+# asd
+
 
 def test_create(driver: webdriver.Chrome):
     """Teszteset létrehozása"""
-    page = LocationsPage(driver)   
-    
-    #when
+    page = LocationsPage(driver)
+
+    # when
     page.click_on_create_location()
-    page.fill_create_form("Home","1,1")
+    page.fill_create_form("Home", "1,1")
     page.click_on_create_location_submit()
-    
-    #then
+
+    # then
     page.wait_for_message("Location has been created")
 
-    name,coords = page.get_first_location()    
+    name, coords = page.get_first_location()
     assert name == "Home"
-    assert coords == "1, 1"   
+    assert coords == "1, 1"
+
 
 def test_negativ(driver: webdriver.Chrome):
-    page = LocationsPage(driver) 
+    page = LocationsPage(driver)
     page.click_on_create_location()
     page.click_on_create_location_submit()
     page.wait_for_name_message("Can not be empty name!")
 
-    #given
+    # given
 
     assert page.has_name_red_border()
     page.wait_for_table_has_rows(0)
-    
+
 
 def test_edit(driver: webdriver.Chrome):
-    #Given: legyen az adatbázisban egy test ... nevű location
-    create_location("Test","5,5")
-    
+    # Given: legyen az adatbázisban egy test ... nevű location
+    create_location("Test", "5,5")
+
     page = LocationsPage(driver)
     page.wait_for_table_has_rows(1)
 
     page.click_on_first_edit_button()
-    page.fill_update_form("Test2","8,8")
+    page.fill_update_form("Test2", "8,8")
     page.click_on_update_location_submit()
 
     page.wait_for_message("Location has been modified")
     page.wait_for_table_has_rows(1)
 
-    name,coords = page.get_first_location()    
+    name, coords = page.get_first_location()
     assert name == "Test2"
-    assert coords == "8, 8" 
+    assert coords == "8, 8"
 
-def test_delete(driver:webdriver.Chrome):
-    create_location("Test","5,5")
-    
+
+def test_delete(driver: webdriver.Chrome):
+    create_location("Test", "5,5")
+
     page = LocationsPage(driver)
     page.wait_for_table_has_rows(1)
     page.click_on_first_delete_button()
@@ -74,16 +79,14 @@ def test_delete(driver:webdriver.Chrome):
     page.wait_for_message("Location has been deleted")
     page.wait_for_table_has_rows(0)
 
+
 def test_with_data_driven(driver):
     page = LocationsPage(driver)
 
-    for name ,coords in read_locations():
+    for name, coords in read_locations():
         page.click_on_create_location()
-        page.fill_create_form(name,coords)
+        page.fill_create_form(name, coords)
         page.click_on_create_location_submit()
         page.wait_for_message("Location has been created")
 
-  
     page.wait_for_table_has_rows(10)
-
-   
